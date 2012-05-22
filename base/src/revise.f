f0d08fae4f770ad71bb830d2298fff3436fddd1e
Nick Crookston <ncrookston.fs@gmail.com>
2012-05-22 20:43:59 +0000

    Added change notes on Climate-FVS (missed them) and updated revise for current date and exdbs to add entry for Climate-FVS database table.
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/trunk@275 90b89b48-9230-0de4-5065-812dd12741b8

--- base/src/revise.f
+++ base/src/revise.f
@@ -1,7 +1,7 @@
       SUBROUTINE REVISE (VAR,REV)
       IMPLICIT NONE
 C----------
-C  **REVISE--BASE  DATE OF LAST REVISION:  03/22/11
+C  **REVISE--BASE  DATE OF LAST REVISION:  2012-05-22
 C  (DON'T CHANGE THIS DATE UNLESS THE SUBROUTINE LOGIC CHANGES.)
 C----------
 C
@@ -15,28 +15,28 @@ C----------
 C SOUTHEAST ALASKA / COASTAL BRITISH COLUMBIA
 C----------
       IF(VAR(:2).EQ.'AK') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C BLUE MOUNTAINS
 C----------
       IF(VAR(:2).EQ.'BM') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C INLAND CALIFORNIA / SOUTHERN CASCADES
 C----------
       IF(VAR(:2).EQ.'CA') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C CENTRAL IDAHO
 C----------
       IF(VAR(:2).EQ.'CI') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
@@ -44,14 +44,14 @@ C CENTRAL ROCKIES
 C----------
       IF(VAR(:2).EQ.'SM' .OR. VAR(:2).EQ.'SP' .OR. VAR(:2).EQ.'BP'
      & .OR. VAR(:2).EQ.'SF' .OR. VAR(:2).EQ.'LP') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C CENTRAL STATES
 C----------
       IF(VAR(:7).EQ.'CS') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
@@ -65,98 +65,98 @@ C----------
 C EASTERN MONTANA
 C----------
       IF(VAR(:2).EQ.'EM') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C INLAND EMPIRE (AKA NI23)
 C----------
       IF(VAR(:2).EQ.'IE') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C KOOTENAI / KANIKSU / TALLY LAKE
 C----------
       IF(VAR(:2).EQ.'KT') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C LAKE STATES
 C----------
       IF(VAR(:7).EQ.'LS') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C KLAMATH MOUNTAINS
 C----------
       IF(VAR(:2).EQ.'NC') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C NORTHEAST STATES
 C----------
       IF(VAR(:7).EQ.'NE') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C NORTH IDAHO
 C----------
       IF(VAR(:2).EQ.'NI') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C PACIFIC NORTHWEST
 C----------
       IF(VAR(:2).EQ.'PN') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C SOUTHERN
 C----------
       IF(VAR(:2).EQ.'SN') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C SOUTH CENTRAL OREGON / NORTHEAST CALIFORNIA
 C----------
       IF(VAR(:2).EQ.'SO') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C TETONS
 C----------
       IF(VAR(:2).EQ.'TT') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C UTAH
 C----------
       IF(VAR(:2).EQ.'UT') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C WEST CASCADES
 C----------
       IF(VAR(:2).EQ.'WC') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C----------
 C WESTERN SIERRA NEVADA
 C----------
       IF(VAR(:2).EQ.'WS') THEN
-        REV = '03/20/12'
+        REV = '2012-05-22'
         GO TO 100
       ENDIF
 C
