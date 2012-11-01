95a7629e5cab092867da82ace8d40c56356778d7
Fred Martin <fred.martin@dnr.wa.gov>
2012-11-01 22:39:17 +0000

    Minor re-arranging code to be more consistent with eccalc.f.
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/branches/dev@385 90b89b48-9230-0de4-5065-812dd12741b8

--- econ/src/ecinit.f
+++ econ/src/ecinit.f
@@ -3,9 +3,7 @@ C----------
 C **ECINIT--ECON  DATE OF LAST REVISION: 09/02/2010
 C----------
 C Author Fred Martin, WA DNR,
-
 ! Initializes defaults for ECON extension variables at beginning of each simulation.
-
 ! Called from INITRE, once for each simulated stand.
 
       implicit none
@@ -13,42 +11,41 @@ C Author Fred Martin, WA DNR,
       include 'PRGPRM.F77'                                               !required by ECNCOM
       include 'ECNCOM.F77'
 
-      integer i
-
 !    Initialize control variables
-      discountRate    =   0.0
-      econStartYear   = -9999                                            !Used to ensure correct start year
+      discountRate    =    0.0
+      econStartYear   =  -9999                                           !Used to ensure correct start year
       noLogStockTable = .false.
       noOutputTables  = .false.
       isEconToBe      = .false.
-      pctMinDbh       =   0.0
-      pctMinUnits     =     0
-      pctMinVolume    =   0.0
+      pctMinDbh       =    0.0
+      pctMinUnits     =      0
+      pctMinVolume    =    0.0
       isFirstEcon     = .true.
       doSev           = .false.
-      sevInput        =   0.0
+      sevInput        =    0.0
       call setPretendStatus()                                            !Entry in ecstatus.f, initializes PRETEND state
 
-!    Initialize volume accumulators.
+!    Initialize volume accumulators.  Set here 1st for use in echarv.f, then re-set each cycle in eccalc.f
       dbhSq     = 0.0
       harvest   = 0.0                                                    !Harvest volume array (TPA : FT3_100)
       hrvCostBf = 0.0; hrvCostFt3 = 0.0; hrvCostTpa = 0.0                !Harvest volume by cost type arrays (1:MAX_KEYWORDS)
+      pctBf     = 0.0; pctFt3     = 0.0; pctTpa     = 0.0                !PCT harvest volume by cost type arrays (1:MAX_KEYWORDS)
+      revVolume = 0.0                                                    !Array(1:MAXSP, 1:MAX_REV_UNITS, 1:MAX_KEYWORDS)
+! Initialize indvidual tree and log accumulatiors. Set heare 1st for use in ecvol.f, then reset by tree in ecvol.f
       logBfVol  = 0.0; logDibBf   = 0.0; logFt3Vol  = 0.0                !Log arrays (1:MAXTRE, 1:MAX_LOGS)
       logDibFt3 = 0.0
-      pctBf     = 0.0; pctFt3     = 0.0; pctTpa     = 0.0                !PCT harvest volume by cost type arrays (1:MAX_KEYWORDS)
 
-!    Initialize revenue arrays
+!    Initialize revenue arrays recieving their value from keyword entries
       hrvRevCnt   =   0                                                  !Array(1:MAXSP, 1:MAX_REV_UNITS)
       hrvRevDia   = 0.0                                                  !Array(1:MAXSP, 1:MAX_REV_UNITS, 1:MAX_KEYWORDS)
       hrvRevPrice = 0.0                                                  !Array(1:MAXSP, 1:MAX_REV_UNITS, 1:MAX_KEYWORDS)
-      revVolume   = 0.0                                                  !Array(1:MAXSP, 1:MAX_REV_UNITS, 1:MAX_KEYWORDS)
-      hrvRevRate  = 0.0                                                  !Array(1:MAXSP, 1:MAX_REV_UNITS, 1:MAX_KEYWORDS, 1:MAX_RATES)
       hrvRevDur   =   0                                                  !Array(1:MAXSP, 1:MAX_REV_UNITS, 1:MAX_KEYWORDS, 1:MAX_RATES)
+      hrvRevRate  = 0.0                                                  !Array(1:MAXSP, 1:MAX_REV_UNITS, 1:MAX_KEYWORDS, 1:MAX_RATES)
 
 !    Initialize cost variable arrays.
-      annCostCnt      =     0
+      annCostCnt      =    0
       annCostAmt      =   0.0                                            !Array(1:MAX_KEYWORDS)
-      annRevCnt       =   0
+      annRevCnt       =     0
       annRevAmt       =   0.0                                            !Array(1:MAX_KEYWORDS)
       burnCostAmt     =   0.0
       fixHrvAmt       =   0.0                                            !Array(1:MAX_KEYWORDS)
@@ -58,17 +55,17 @@ C Author Fred Martin, WA DNR,
       mechCostAmt     =   0.0
       plntCostCnt     =     0
       plntCostAmt     =   0.0                                            !Array(1:MAX_PLANT_COSTS)
-      plntCostUnits   =   0.0                                            !Array(1:MAX_PLANT_COSTS)
+      plntCostUnits   =     0                                            !Array(1:MAX_PLANT_COSTS)
       varHrvAmt       =   0.0                                            !Array(1:MAX_KEYWORDS)
       varHrvCnt       =     0
       varHrvDbhLo     =   0.0                                            !Array(1:MAX_KEYWORDS)
       varHrvDbhHi     = 999.0                                            !Array(1:MAX_KEYWORDS)
-      varHrvUnits     =   0.0                                            !Array(1:MAX_KEYWORDS)
+      varHrvUnits     =     0                                            !Array(1:MAX_KEYWORDS)
       varPctAmt       =   0.0                                            !Array(1:MAX_KEYWORDS)
       varPctCnt       =     0
       varPctDbhLo     =   0.0                                            !Array(1:MAX_KEYWORDS)
       varPctDbhHi     = 999.0                                            !Array(1:MAX_KEYWORDS)
-      varPctUnits     =   0.0                                            !Array(1:MAX_KEYWORDS)
+      varPctUnits     =     0                                            !Array(1:MAX_KEYWORDS)
 
 !    Initialize cost & rate/duration arrays, needed as not all elements may have values assigned.
       annCostRate  = 0.0                                                 !Array(1:MAX_KEYWORDS, 1:MAX_RATES)
