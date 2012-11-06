d3bb0ac00da21bc6036e844efc3d8ad8baddf86f
Don Robinson <drobinsonessa@gmail.com>
2012-11-06 05:26:00 +0000

    remove reference to GLBLCNTL.F77; add argument to CMDLINE call; remove call to GRSTOP; makes MAIN independent of F77 common block
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/branches/dev@422 90b89b48-9230-0de4-5065-812dd12741b8

--- base/src/main.f
+++ base/src/main.f
@@ -3,23 +3,20 @@
 C----------
 C  **MAIN--BASE DATE OF LAST REVISION:  10/13/2012
 C----------
-      include "GLBLCNTL.F77"
       INTEGER rtnCode,lenCL
 C
 C     PROCSS THE COMMAND LINE. Passing an empty string signals that the 
 C     real command line arguments will be fetched.
 C
       lenCl = 0
-      CALL cmdline(' ',lenCL)
-      
-      IF (fvsRtnCode.NE.0) GOTO 10
+      CALL cmdLine(' ',lenCL,rtnCode)
+      IF (rtnCode.NE.0) GOTO 10
 C
 C     INITIALIZE THE MULTIPLE REPORT ROUTINE (THIS DOES NOT OPEN A FILE)
 C
       CALL GENRPT
 
 C     RUN ALL THE CYCLES and STANDS--unless there is a stop point!
-
       DO
         CALL FVS(rtnCode)
         IF (rtnCode .NE. 0) exit
@@ -27,9 +24,6 @@ C     RUN ALL THE CYCLES and STANDS--unless there is a stop point!
 
    10 CONTINUE     
 
-C     This call does not return.
-
-      CALL GRSTOP
       STOP
       END      
 
