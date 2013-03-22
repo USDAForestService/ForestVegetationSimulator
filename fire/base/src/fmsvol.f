e8bc89b5600e46ad40d64715f51376c6f2ead797
Don Robinson <drobinsonessa@gmail.com>
2013-03-22 23:17:42 +0000

    give some small volume to very small trees
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/branches/dev@737 90b89b48-9230-0de4-5065-812dd12741b8

--- fire/base/src/fmsvol.f
+++ fire/base/src/fmsvol.f
@@ -53,7 +53,7 @@ C.... VARIABLE DECLARATIONS.
       INTEGER  JS, ISPC, IT
       REAL     D, H, BARK, XHT, VOL2HT
       INTEGER  IOUT,II,JSP,IHT
-      REAL     XH,XD,BRATIO,D2H,BBFV
+      REAL     XH,XD,BRATIO,D2H,BBFV,X
 
 C     CALCULATE THE VOLUME
 
@@ -115,7 +115,11 @@ C     TKILL = LTKIL
       IF(CTKFLG .AND. LTKIL)
      1     CALL CFTOPK (ISPC,D,H,VN,VM,VMAX,LCONE,BARK,IHT)
 
-      VOL2HT = VN
+C     Give some small volume to very tiny trees.
+C     based on cone with D = 1"
+      X = 0.005454154 * H
+      VOL2HT = MAX(X,VN)
+      
       IF (LMERCH) VOL2HT = VM
 
       IF (DEBUG) WRITE(IOUT,40)ISPC,D,H,LCONE,VN
