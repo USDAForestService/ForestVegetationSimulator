9453fc337dffd55bccc8ebce35e69ff5a095976e
Robert Havis <rhavis122@gmail.com>
2013-10-31 15:57:23 +0000

    reved back the test
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/trunk@1069 90b89b48-9230-0de4-5065-812dd12741b8

--- ak/src/grinit.f
+++ ak/src/grinit.f
@@ -1,7 +1,7 @@
       SUBROUTINE GRINIT
       IMPLICIT NONE
 C----------
-$Id$
+C AK $Id$
 C----------
 C
 C  INITIALIZE PROGNOSIS MODEL VARIABLES
@@ -253,7 +253,6 @@ C----------
       DBHSTAGE=0.
       DR016=0.
       DBHSDI=0.
-      JSPINDEF=0
 C
       DO 30 J=1,9
       DO 20 K=1,MAXSP
