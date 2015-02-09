35a8b007d85295fb1a8b99d74c94c7a473a074c9
Nick Crookston <ncrookston.fs@gmail.com>
2015-02-09 17:59:18 +0000

    fvsSQL.c: added some debug code that is commented out. dbscmpu.f: fixed several datatype definition problems but the biggest change was to add a query that   results in getting correct column counts on existing FVS_Compute tables. This problem was introduced   with the update that improved speed a few months ago.
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/trunk@1425 90b89b48-9230-0de4-5065-812dd12741b8

--- dbs/src/dbscmpu.f
+++ dbs/src/dbscmpu.f
@@ -40,9 +40,10 @@ C
       REAL,DIMENSION(MXTST5)::KWVALS
       INTEGER NUMKW,I,I1,I2,I3,I4,INSRTNUM,II,X,ICY,THISYR,IRCODE
       LOGICAL KWINLIST,LDUPKW
-      INTEGER(SQLSMALLINT_KIND)::ColNumber,NameLen,ColumnCount,DTp,
+      INTEGER(SQLUSMALLINT_KIND)::ColNumber
+      INTEGER(SQLSMALLINT_KIND)::NameLen,ColumnCount,DTp,
      -       NDecs, Nullable
-      INTEGER(SQLUINTEGER_KIND) NColSz
+      INTEGER(SQLULEN_KIND) NColSz
       NUMKW = ITST5
 C
 C     IF COMPUTE IS NOT TURNED ON OR THE NUMBER OF VARIABLES IS 0
@@ -136,15 +137,21 @@ C
       ELSE
 C
 C       POPULATE THE KWLIST WITH THE CURRENT COLUMN NAMES
+C       FIRST, FETCH ALL THE COLS SO WE CAN GET THE NAMES
 C
+        SQLStmtStr= 'SELECT * FROM FVS_Compute'
+        iRet = fvsSQLExecDirect(StmtHndlOut,trim(SQLStmtStr),
+     >            int(len_trim(SQLStmtStr),SQLINTEGER_KIND))
         iRet = fvsSQLNumResultCols(StmtHndlOut,ColumnCount)
-
+c*       print *,"iRet=",iRet," ColumnCount=",ColumnCount
         DO ColNumber = 1,ColumnCount
           iRet = fvsSQLDescribeCol (StmtHndlOut, ColNumber, ColName,
-     -         int(LEN(ColName),SQLUSMALLINT_KIND), NameLen, DTp,
-     -         NColSz, NDecs, Nullable)
-          COLNAME(NameLen+1:)=' '
-          IF (ColNumber.GT.3) KWLIST(ColNumber-3) = COLNAME
+     -         int(LEN(ColName),SQLSMALLINT_KIND), NameLen, DTp,
+     -         NColSz, NDecs, Nullable)     
+          ColName(NameLen+1:)=' '
+c*        print *,"ColNumber=",ColNumber," iRet=",iRet,
+c*     >   " NameLen=",NameLen," ColName=",ColName
+          IF (ColNumber.GT.3) KWLIST(ColNumber-3) = ColName(:NameLen)
         ENDDO
         NUMKW = ColumnCount - 3
       ENDIF
@@ -205,19 +212,19 @@ C
                 ENDIF
               ENDDO
             ENDIF
-            
+             
             !IF KW NOT IN LIST THEN DETERMINE IF WE WANT TO ALTER TABLE
-            IF((.NOT.KWINLIST).AND.(IADDCMPU.LT.1)) THEN
-              IF(TRIM(DBMSOUT).NE.'EXCEL')THEN       
-                SQLStmtStr='ALTER TABLE '//trim(TABLENAME)//' ADD '//
+            IF((.NOT.KWINLIST).AND.TRIM(DBMSOUT).NE.'EXCEL'
+     -          .AND.IADDCMPU.LT.1) THEN
+              SQLStmtStr='ALTER TABLE '//trim(TABLENAME)//' ADD '//
      -          LWRAP//TRIM(KEYWRD)//RWRAP//' '//trim(DTYPE)//' null'
-                !Close Cursor
-                iRet = fvsSQLCloseCursor(StmtHndlOut)
-                iRet = fvsSQLExecDirect(StmtHndlOut,trim(SQLStmtStr),
+              !Close Cursor
+              iRet = fvsSQLCloseCursor(StmtHndlOut)
+              iRet = fvsSQLExecDirect(StmtHndlOut,trim(SQLStmtStr),
      -                int(len_trim(SQLStmtStr),SQLINTEGER_KIND))
-                CALL DBSDIAGS(SQL_HANDLE_STMT,StmtHndlOut,
+              CALL DBSDIAGS(SQL_HANDLE_STMT,StmtHndlOut,
      -            'DBSCMPU:Alter Table: '//trim(SQLStmtStr))
-              ENDIF
+
               !ADD NEW COLUMN TO MASTER LIST
               NUMKW = NUMKW+1
               KWLIST(NUMKW) = KEYWRD
