784f01ebacb9d75cf122da87ac3898db8d9ffe9b
Tod Haren <tod.haren@gmail.com>
2015-03-24 22:04:15 +0000

    Merge with trunk, SVN r1453.
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/branches/PyFVS@1455 90b89b48-9230-0de4-5065-812dd12741b8

--- base/src/main.f
+++ base/src/main.f
@@ -4,27 +4,6 @@ C----------
 C  $Id$
 C----------
       INTEGER rtnCode,lenCL,i
-
-#ifdef PROFILING
-      ! In profiling mode multiple iterations of the same keyword
-      ! file can be run to increase the sample size for gprof
-      integer :: iters,j
-      character(len=32) :: arg
-
-      iters = 1
-      do j=0, command_argument_count()
-        call get_command_argument(j,arg)
-        if (len_trim(arg)==0) exit
-        if ((arg=='--iters') .or. (arg=='-i')) then
-            call get_command_argument(j+1,arg)
-            read(arg, '(i10)'), iters
-            print '(a i4)', 'Number of iterations:',iters
-        endif
-      enddo
-
-      do j=1, iters
-#endif
-
 C
 C     PROCSS THE COMMAND LINE. Passing an empty string signals that the 
 C     real command line arguments will be fetched.
@@ -44,13 +23,7 @@ C     RUN ALL THE CYCLES and STANDS--unless there is a stop point!
    
       call fvsGetICCode(i)
 
-#ifdef PROFILING
-	      print '(a i4)',"End of run: ",j
-	      if (i > 0) exit
-      enddo
-#endif
-
-      IF (i .EQ. 0) STOP
+      IF (i .EQ. 0) STOP	
 
       GO TO (11,12,13,14,15), i 
    11 CONTINUE
