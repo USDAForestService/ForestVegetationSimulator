bc8f432eeb5bc9ead72a578726404e6c0949859d
Nick Crookston <ncrookston.fs@gmail.com>
2018-02-08 18:22:30 +0000

    Changed program names (prepend q rather than adding Q), modified makefile per Lance's change (on top of my changes), added some comments to dbsinit.f
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/branches/NickDev@2095 90b89b48-9230-0de4-5065-812dd12741b8

--- dbsqlite/dbsinit.f
+++ dbsqlite/dbsinit.f
@@ -5,6 +5,38 @@ C $Id$
 C
       INCLUDE 'DBSCOM.F77'
 C
+C  Table name           Subroutine  Control Var  Organization
+C  ==================== =========== ===========  ============
+C  FVS_ATRTList         dbsatrtls   IATRTLIST    cycle, tree
+C  FVS_Cases            dbscase     CASEID       cycle 
+C  FVS_Climate          dbsclsum    ICLIM        cycle, species
+C  FVS_Compute          dbscmpu     ICOMPUTE     cycle 
+C  FVS_CutList          dbscuts     ICUTLIST     cycle, tree
+C  FVS_EconHarvestValue dbsecharv   IDBSECON     cycle 
+C  FVS_EconSummary      dbsecsum    IDBSECON     cycle 
+C  FVS_BurnReport       dbsfmburn   IBURN        cycle             
+C  FVS_CanProfile       dbsfmcanpr  ICANPR       cycle, height class
+C  FVS_Carbon           dbsfmcrpt   ICMRPT       cycle 
+C  FVS_SnagDet          dbsfmdsnag  ISDET        cycle, species, dbhclass
+C  FVS_Down_Wood_Cov    dbsfmdwcov  IDWDCOV      cycle 
+C  FVS_Down_Wood_Vol    dbsfmdwvol  IDWDVOL      cycle 
+C  FVS_Consumption      dbsfmfuel   IFUELC       cycle 
+C  FVS_Hrv_Carbon       dbsfmhrpt   ICHRPT       cycle 
+C  FVS_Mortality        dbsfmmort   IMORTF       cycle 
+C  FVS_PotFire_East     dbsfmpf     IPOTFIRE     cycle           
+C  FVS_PotFire          dbsfmpf     IPOTFIRE     cycle 
+C  FVS_SnagSum          dbsfmssnag  ISSUM        cycle 
+C  FVS_Fuels            dbsfuels    IFUELS       cycle 
+C  FVS_DM_Spp_Sum       dbsmis      IDM1         cycle, species
+C  FVS_DM_Stnd_Sum      dbsmis      IDM2         cycle 
+C  FVS_DM_Sz_Sum        dbsmis      IDM3         cycle 
+C  FVS_RD_Sum           dbsrd       IRD1         cycle 
+C  FVS_RD_Det           dbsrd       IRD2         cycle 
+C  FVS_RD_Beetle        dbsrd       IRD3         cycle 
+C  FVS_StrClass         dbsstrclass ISTRCLAS     cycle 
+C  FVS_Summary_East     dbssumry    ISUMARY      cycle 
+C  FVS_Summary          dbssumry    ISUMARY      cycle 
+C  FVS_TreeList         dbstrls     ITREELIST    cycle, tree  
 C
       CASEID    = ""
       ISUMARY  = 0
