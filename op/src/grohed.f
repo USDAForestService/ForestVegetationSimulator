d5226b1795478e3c3a7d07821bd3cede135a0293
Nick Crookston <ncrookston.fs@gmail.com>
2018-04-13 09:54:57 +0000

    NickDev: Fixed the headings for oc and op
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/branches/NickDev@2200 90b89b48-9230-0de4-5065-812dd12741b8

--- op/src/grohed.f
+++ op/src/grohed.f
@@ -1,11 +1,7 @@
       SUBROUTINE GROHED (IUNIT)
       IMPLICIT NONE
 C----------
-C  **GROHED -- OP   DATE OF LAST REVISION:  05/1/15
-C $Id: grohed.f 167 2012-04-14 15:58:06Z jdh $
-C $Revision: 167 $
-C $Date: 2012-04-14 08:58:06 -0700 (Sat, 14 Apr 2012) $
-C $HeadURL: https://www.forestinformatics.com/svn/fvs/trunk/pncor/src/grohed.f $
+C $Id$ 
 C----------
 C     WRITES HEADER FOR BASE MODEL PORTION OF PROGNOSIS SYSTEM
 C----------
@@ -35,7 +31,7 @@ C
 C
       WRITE (IUNIT,40) SVN,REV,DAT,TIM
    40 FORMAT (//T10,'FOREST VEGETATION SIMULATOR',
-     >  5X,'VERSION ',A,' – ORGANON NWO&SMC',
+     >  5X,'VERSION ',A,' ORGANON NWO&SMC',
      >  T97,'RV:',A,T112,A,2X,A)
 C
       RETURN
