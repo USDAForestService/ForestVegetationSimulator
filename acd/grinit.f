2f6cb5da1a0a5233048efef4b4f92a2869ad70c3
Gary Dixon <noreply@null.com>
2018-06-04 15:22:56 +0000

    change /ci/src to /ci
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/branches/GaryDev@2381 90b89b48-9230-0de4-5065-812dd12741b8

--- acd/grinit.f
+++ acd/grinit.f
@@ -66,7 +66,7 @@ C----------
       DATA DBLK/'                          '/
       DATA NONE/'NONE'/
 C----------
-      VARIANT = 'AN'
+      VARIANT = 'AC'
       CALL LNKINT
       DO 5 I=1,MAXSP
       SDIDEF(I) = 0.
