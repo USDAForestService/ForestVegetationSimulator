1b287966cb216977fbfc5bf2af7a0a319db7765f
Nick Crookston <ncrookston.fs@gmail.com>
2019-03-08 18:45:42 +0000

    Better test of FVS_Summary2 and FVS_Summary.
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/trunk@2621 90b89b48-9230-0de4-5065-812dd12741b8

--- tests/qFVSie/sqliteTestFVSieOut.sql
+++ tests/qFVSie/sqliteTestFVSieOut.sql
@@ -4,7 +4,8 @@ select StandID, RunTitle, KeywordFile, SamplingWt, Variant from FVS_Cases;
 select StandID, Year, round(MYBBA,2), round(MYABA,2), round(MYBAR,2), 
   round(newDHt,2), round(newMyBBA,2), round(X1,2)
   from FVS_Compute;
-select Count(*) from FVS_TreeList;
-select StandID, TopHt, BA, TPA from FVS_Summary;
+select "Treelist count" as cnt,Count(*) from FVS_TreeList;
+select "from summary " as frm,StandID, Year, Age, TopHt, BA, TPA from FVS_Summary;
+select "from summary2" as frm,StandID, Year, Age, TopHt, round(BA,0), round(TPA,0) from FVS_Summary2;
 select StandID, Year, round(Inf_TPA,0) as Inf_TPA from FVS_DM_Stnd_Sum;
 select StandID, Year, Spp, round(Inf_TPA,0) as Inf_TPA from FVS_DM_Spp_Sum;
