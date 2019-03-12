aa57dfd33de77a4636a4275c280a1f853d41872d
Lance David <lance.david23@gmail.com>
2019-03-12 21:59:17 +0000

    Dead trees were missing from cycle 0 output and TreeId numbers incorrect due to missing parameter on first section binding of TID at line 235.
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/trunk@2622 90b89b48-9230-0de4-5065-812dd12741b8

--- dbsqlite/dbstrls.f
+++ dbsqlite/dbstrls.f
@@ -232,7 +232,8 @@ C
             IF(ISPOUT6.EQ.3)CSPECIES=ADJUSTL(TRIM(PLNJSP(ISP(I)))) 
             
             ColNumber=1
-            iRet = fsql3_bind_int(IoutDBref,ColNumber,TID)
+            iRet = fsql3_bind_text(IoutDBref,ColNumber,TID,
+     >                            LEN_TRIM(TID))
             ColNumber=ColNumber+1
             iRet = fsql3_bind_int(IoutDBref,ColNumber,I)
             ColNumber=ColNumber+1
@@ -308,12 +309,10 @@ C     FOR CYCLE 0 TREELIST, PRINT DEAD TREES WHICH WERE PRESENT IN
 C     THE INVENTORY DATA AT THE BOTTOM OF THE TREELIST.
 C
       IF (ITREELIST .EQ. 0) RETURN
-      IF(.NOT.((IREC2.GE.MAXTP1).OR.(ITPLAB.EQ.3).OR.
-     >         (ICYC.GE.1))) THEN
-        iRet = fsql3_finalize(IoutDBref)
-        RETURN
+      IF((IREC2.GE.MAXTP1).OR.(ITPLAB.EQ.3).OR.(ICYC.GE.1)) THEN
+        go to 100
       ENDIF
-      
+
       DO I=IREC2,MAXTRE
         P =(PROB(I) / GROSPC) / (FINT/FINTM)
         WRITE(TID,'(I8)') IDTREE(I)
@@ -436,7 +435,7 @@ C
         iRet = fsql3_step(IoutDBref)
         iRet = fsql3_reset(IoutDBref)
       ENDDO
-      iRet = fsql3_finalize(IoutDBref)
+  100 iRet = fsql3_finalize(IoutDBref)
       iRet = fsql3_exec (IoutDBref,"Commit;"//Char(0))
 
       RETURN
