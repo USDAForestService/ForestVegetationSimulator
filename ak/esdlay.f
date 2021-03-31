18bfd82f6cd160eddb7e6f1d3883c16602ffedfe
Chad Keyser <chad.keyser@usda.gov>
2021-03-31 13:54:08 +0000

    Removed budworm effect on establishment delay times in AK.
    git-svn-id: svn+ssh://svn.code.sf.net/p/open-fvs/code/branches/AKdev@3476 90b89b48-9230-0de4-5065-812dd12741b8

--- ak/esdlay.f
+++ ak/esdlay.f
@@ -29,12 +29,12 @@ C----------
 C  VARIABLE DECLARATIONS:
 C----------
 C
-      INTEGER IAS,ISPE,IT,IBW,IB,IBAA,I
+      INTEGER IAS,ISPE,IT,IBAA
 C
       REAL BB,CC,DELAY,DRAW
 C
-      REAL BADV(2,MAXSP),BBW(3,4),BBW1(2,4),BBW2(2,4),BSUB(3,MAXSP)
-      REAL CADV(2,MAXSP),CBW(3,4),CBW1(2,4),CBW2(2,4),CSUB(3,MAXSP)
+      REAL BADV(2,MAXSP),BSUB(3,MAXSP)
+      REAL CADV(2,MAXSP),CSUB(3,MAXSP)
 C
 C----------
 C     PLOT AGE (YRS): 2 THRU 7   8 THRU 12   13 THRU 20
@@ -65,12 +65,6 @@ C
      &                3.81610,   5.74622,     9.36345, !   22  SU = Scouler's willow
      &                3.81610,   5.74622,     9.36345/ ! # 23  OH = other hardwoods
 C
-C     ONLY ORIGINAL SPECIES, NONE ADDED FOR 23 SPECIES EXPANSION.
-      DATA BBW/       4.01218,   5.86172,    10.61297, !  1 SF = pacific silver fir
-     &                4.11555,   6.19124,     8.82962, !  2 AF = subalpine fir
-     &                5.92251,   8.62548,     8.76074, !  8 SS = sitka spruce
-     &                3.67409,   6.12256,     8.74195/ ! 12 MH = mountain hemlock
-C
 C     PLOT AGE (YRS): 3 THRU 7   8 THRU 12   13 THRU 20
 C     ORIGINAL VALUES FROM 13 SPECIES AK MARKED WITH #
 C
@@ -99,14 +93,7 @@ C
      &                3.01975,   2.09376,     1.80925, !   22  SU = Scouler's willow
      &                3.01975,   2.09376,     1.80925/ ! # 23  OH = other hardwoods
 C
-C     ONLY ORIGINAL SPECIES, NONE ADDED FOR 23 SPECIES EXPANSION.
-      DATA CBW/       2.16561,   2.16010,     2.44953, !  1 SF = pacific silver fir
-     &                1.98007,   2.84149,     2.46665, !  2 AF = subalpine fir
-     &                3.31325,   5.21269,     2.30270, !  8 SS = sitka spruce
-     &                2.19139,   2.38522,     2.03146/ ! 12 MH = mountain hemlock
-C
 C     OVERSTORY BA:  0 TO 25  26+ SQ.FT./A.
-C     NO BUDWORM BEFORE HARVEST
 C     ORIGINAL VALUES FROM 13 SPECIES AK MARKED WITH #
 C
       DATA BADV/
@@ -134,23 +121,6 @@ C
      &              8.986115, 10.312660, !   22  SU = Scouler's willow
      &              8.986115, 10.312660/ ! # 23  OH = other hardwoods
 C
-C     BUDWORM 1-3 YEARS BEFORE HARVEST
-C     ONLY ORIGINAL SPECIES, NONE ADDED FOR 23 SPECIES EXPANSION.
-C
-      DATA BBW1/   16.856252, 18.601097, !  1 SF = pacific silver fir
-     &             20.207660, 30.052681, !  2 AF = subalpine fir
-     &              8.388204, 18.023409, !  8 SS = sitka spruce
-     &             11.393035, 19.994331/ ! 12 MH = mountain hemlock
-C
-C     BUDWORM 4-5 YEARS BEFORE HARVEST
-C     ONLY ORIGINAL SPECIES, NONE ADDED FOR 23 SPECIES EXPANSION.
-C
-      DATA BBW2/   18.833076, 17.660965, !  1 SF = pacific silver fir
-     &             25.512363, 20.555202, !  2 AF = subalpine fir
-     &             10.587442, 16.554662, !  8 SS = sitka spruce
-     &             16.154050, 13.936793/ ! 12 MH = mountain hemlock
-C
-C     NO BUDWORM BEFORE HARVEST
 C     ORIGINAL VALUES FROM 13 SPECIES AK MARKED WITH #
 C
       DATA CADV/
@@ -178,77 +148,22 @@ C
      &              1.068472,  1.470405, !   22  SU = Scouler's willow
      &              1.068472,  1.470405/ ! # 23  OH = other hardwoods
 C
-C     BUDWORM 1-3 YEARS BEFORE HARVEST
-C     ONLY ORIGINAL SPECIES, NONE ADDED FOR 23 SPECIES EXPANSION.
-C
-      DATA CBW1/    1.165449,  1.182513, !  1 SF = pacific silver fir
-     &              1.660504,  1.832537, !  2 AF = subalpine fir
-     &              1.332778,  1.502281, !  8 SS = sitka spruce
-     &              1.137030,  1.355774/ ! 12 MH = mountain hemlock
-C
-C     BUDWORM 4-5 YEARS BEFORE HARVEST
-C     ONLY ORIGINAL SPECIES, NONE ADDED FOR 23 SPECIES EXPANSION.
-C
-      DATA CBW2/    1.307195,  1.938481, !  1 SF = pacific silver fir
-     &              1.871949,  1.248784, !  2 AF = subalpine fir
-     &              1.470496,  1.207402, !  8 SS = sitka spruce
-     &              1.330297,  1.311782/ ! 12 MH = mountain hemlock
 C----------
       IT=1
       IF(TIME.GT.7.5.AND.TIME.LT.12.5) IT=2
       IF(TIME.GT.12.5) IT=3
-      IBW = INT(BWB4+BWAF+0.5)
-      IB=1
-      IF(IBW.GT.2) IB=2
       IBAA=1
       IF(BAA.GT.25.5) IBAA=2
 C     ADVANCE REGENERATION
       IF(IAS .EQ. 1) THEN
         BB=BADV(IBAA,ISPE)
         CC=CADV(IBAA,ISPE)
-        IF(BWB4.LT.0.5) GO TO 40
-        SELECT CASE (ISPE)
-        CASE(1)  ! SF
-          I = 1
-        CASE(2)  ! AF
-          I = 2
-        CASE(8)  ! SS
-          I = 3
-        CASE(12) ! MH
-          I = 4
-        CASE DEFAULT
-          GO TO 40
-        END SELECT
-        IF(BWB4 .LE. 3.5) THEN
-          BB=BBW1(IBAA,I)
-          CC=CBW1(IBAA,I)
-        ELSE
-          BB=BBW2(IBAA,I)
-          CC=CBW2(IBAA,I)
-        ENDIF
-   40   CONTINUE
         DELAY=((-(ALOG(1.0-DRAW)))**(1.0/CC))*BB
         DELAY= (DELAY+3.0) *(-1.0)
 C     SUBSEQUENT REGENERATION
       ELSE
         BB=BSUB(IT,ISPE)
         CC=CSUB(IT,ISPE)
-        IF(IB.NE.2) GO TO 30
-        SELECT CASE (ISPE)
-        CASE(1)  ! SF
-          I = 1
-        CASE(2)  ! AF
-          I = 2
-        CASE(8)  ! SS
-          I = 3
-        CASE(12) ! MH
-          I = 4
-        CASE DEFAULT
-          GO TO 30
-        END SELECT
-        BB=BBW(IT,I)
-        CC=CBW(IT,I)
-   30   CONTINUE
         DELAY=((-(ALOG(1.0-DRAW)))**(1.0/CC))*BB
         DELAY=DELAY-4.0
       ENDIF
