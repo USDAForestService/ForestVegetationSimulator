3fb2520e9228ac55bce50e6a387c8dfffd7907e7
Daniel Wagner <daniel.s.wagner@usda.gov>
2022-09-20 11:32:21 -0400

    Changed Revise.f REV variable to date of release "20220930" Changed INCLUDESSVN.F SVN variable to "FS2022.4"

--- vbase/revise.f
+++ vbase/revise.f
@@ -18,117 +18,117 @@ C----------
 C SOUTHEAST ALASKA / COASTAL BRITISH COLUMBIA
 C----------
         CASE('AK')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C BLUE MOUNTAINS
 C----------
         CASE('BM')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C INLAND CALIFORNIA / SOUTHERN CASCADES
 C----------
         CASE('CA')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C CENTRAL IDAHO
 C----------
         CASE('CI')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C CENTRAL ROCKIES
 C----------
         CASE('CR')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C CENTRAL STATES
 C----------
         CASE('CS')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C EAST CASCADES
 C----------
         CASE('EC')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C EASTERN MONTANA
 C----------
         CASE('EM')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C INLAND EMPIRE (AKA NI23)
 C----------
         CASE('IE')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C KOOTENAI / KANIKSU / TALLY LAKE
 C----------
         CASE('KT')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C LAKE STATES
 C----------
         CASE('LS')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C KLAMATH MOUNTAINS
 C----------
         CASE('NC')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C NORTHEAST STATES
 C----------
         CASE('NE')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C SOUTHWEST OREGON ORGANON
 C----------
         CASE('OC')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C NORTHWEST OREGON ORGANON
 C----------
         CASE('OP')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C PACIFIC NORTHWEST
 C----------
         CASE('PN')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C SOUTHERN
 C----------
         CASE('SN')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C SOUTH CENTRAL OREGON / NORTHEAST CALIFORNIA
 C----------
         CASE('SO')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C TETONS
 C----------
         CASE('TT')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C UTAH
 C----------
         CASE('UT')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C WEST CASCADES
 C----------
         CASE('WC')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C WESTERN SIERRA NEVADA
 C----------
         CASE('WS')
-        REV = 'FS2022.4'
+        REV = '20220930'
 C----------
 C ANY OTHER VARIANT
 C----------
         CASE DEFAULT
-        REV = 'FS2022.4'
+        REV = '20220930'
 C
       END SELECT
 C
